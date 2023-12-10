9412c5ba7333cbccb6f15c5938d5c6bfc67c3827
Omar Facundo <114097196+omar5o9@users.noreply.github.com>
2023-12-10 03:12:18 +0000

    Some config

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,11 +2,11 @@ import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Chris Williams",
+	author: "Omar Facundo",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Astro Theme Cactus",
+	title: "Soccer Blog",
 	// Meta property used as the default description meta property
-	description: "An opinionated starter theme for Astro",
+	description: "My Blog about Soccer",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
